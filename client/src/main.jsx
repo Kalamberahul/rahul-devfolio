ef1414e40c298461fbe9a7778ea4fc9fdaadea0d
Kalamberahul <kalamberahul22@gmail.com>
2025-07-23 08:43:37 +0530

    Final Fix: Revert to standard Vite CSS import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- ADD THIS LINE
+import './index.css' // <-- ADD THIS LINE BACK
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
